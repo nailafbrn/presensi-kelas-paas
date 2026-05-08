9be8e08951ae5f1304211ddc81ee153d8816cd33
nailafbrn <nailafw27@gmail.com>
2026-05-08 20:31:53 +0700

    Fix table name consistency

--- database/migrations/2026_05_08_112032_create_presensi_table.php
+++ database/migrations/2026_05_08_112032_create_presensi_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
 {
-    Schema::create('presensis', function (Blueprint $table) {
+    Schema::create('presensi', function (Blueprint $table) {
         $table->id();
         $table->string('nama_mahasiswa');
         $table->string('nim');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('presensis');
+        Schema::dropIfExists('presensi');
     }
 };
